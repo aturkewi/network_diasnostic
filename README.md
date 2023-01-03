bad9eb2f036f418a8ec7a0a7f2ea5a2a66f55946
Avi Turkewitz <aviturkewitz@gmail.com>
2023-01-03 10:42:28 -0500

    fix logging so that it does not overwrite file

--- README.md
+++ README.md
@@ -8,6 +8,4 @@ Logs pings to a CSV
 - `python check_ping.py`
 - `ctrl+c` when ready to quit
 
-ISO datetime and ping will be stored in `network_log.csv`.
-
-**Note, every time program is started, this file is deleted!**
+ISO datetime and ping will be stored in `network_log_{timestamp}.csv`.
--- check_ping.py
+++ check_ping.py
@@ -3,8 +3,9 @@ import csv
 import time
 from datetime import datetime
 
-def file_name():
-  return 'network_log.csv'
+def get_file_name():
+  timestamp = datetime.now().isoformat().split('.')[0]
+  return f"network_log_{timestamp}.csv"
 
 def get_raw_ping_data():
   output = subprocess.check_output(["ping", "-c", "1", "8.8.8.8"])
@@ -17,24 +18,25 @@ def get_time_string(raw_ping_string):
   end = start + 6
   return raw_ping_string[start:end]
 
-def write_to_file(time):
-  with open(file_name(), 'a') as file:
+def write_to_file(time, file_name):
+  with open(file_name, 'a') as file:
     writer = csv.writer(file)
     writer.writerow([datetime.now().isoformat(), time])
 
-def ensure_file():
-  with open(file_name(), 'w') as file:
+def ensure_file(file_name):
+  with open(file_name, 'w') as file:
     writer = csv.writer(file)
     writer.writerow(["timestamp", "ping"])
 
 
 def run():
-  ensure_file()
+  file_name = get_file_name()
+  ensure_file(file_name)
 
   while True:
     raw_ping_string = get_raw_ping_data()
     ping = get_time_string(raw_ping_string)
-    write_to_file(ping)
+    write_to_file(ping, file_name)
     time.sleep(1)
 
 run()
